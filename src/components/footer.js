be5fb8ff9a48de43be8d5253090462ecd0a7f6e3
Joshua Wootonn <joshuawootonn@gmail.com>
2018-05-28 14:17:16 -0500

    Part 1 of the footer done now

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,34 +7,39 @@ const Footer = ({ siteTitle }) => (
   <div className="footer">
 
     <div className="footer-connect">
-      <div className="container columns is-centered is-multiline">
-        <div className="column is-4-tablet is-offset-2 is-offset-2-mobile is-8-mobile">
-          <h4>Location</h4>
-          <p>845 Pepperwood Ln, Iowa City, IA 52240</p>
-          <p>Phone: (319) 354-3331</p>
-        </div>
-        <div className="column is-4-tablet is-offset-2-mobile is-8-mobile">
-          <div className="social">
-            <a href>
-              <img src={gmail} />
-            </a> <a href>
-              <img src={instagram} />
-            </a> <a href>
-              <img src={facebook} />
-            </a>
+      <div className="container ">
+        <div className="column cols is-8 is-offset-2">
+          <div className="col">
+            <div>
+              <h4>Location</h4>
+              <p>845 Pepperwood Ln, Iowa City, IA 52240</p>
+              <p>Phone: (319) 354-3331</p>
+            </div>
           </div>
+          <div className="col">
+            <div className="social">
+              <a href>
+                <img src={gmail} />
+              </a> <a href>
+                <img src={instagram} />
+              </a> <a href>
+                <img src={facebook} />
+              </a>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
     <div className="footer-links">
-      <div className="container columns">
+      <div className="container">
         <div className="column is-8 is-offset-2">
           <p>All right reserved - Good News Bible Church</p>
         </div>
       </div>
     </div>
     <div className="footer-reserved">
-      <div className="container columns">
+      <div className="container">
         <div className="column is-8 is-offset-2">
           <p>All right reserved - Good News Bible Church</p>
         </div>
